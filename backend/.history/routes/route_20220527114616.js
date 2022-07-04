6ab9b045d286345af2ec5b994ce9d000cee50444
rahulnag7963 <69950566+rahulnag7963@users.noreply.github.com>
2022-07-03 17:26:51 -0700

    Delete backend directory

--- backend/.history/routes/route_20220527114616.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Product = require('../model/model.js');
-const productController = require('../controllers/controller');
-
-router.get("/",productController.getAllModels);
-
-module.exports = router;
--- backend/.history/routes/route_20220529191540.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Product = require("../model/model");
-const productController = require("../controllers/controller");
-
-router.get("/", productController.getAllModels);
-router.post("/", productController.addProduct);
-router.get("/:id", productController.getId);
-module.exports = router;
